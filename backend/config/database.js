a4afa70aecbb6c8148b74a817cc594ade8047eb1
Nitish chaurasiya <nitishchaurasiyac555@gmail.com>
2023-07-14 19:20:05 +0530

    err-handler-mongo-log-productid-mongo_err-complet

--- backend/config/database.js
+++ backend/config/database.js
@@ -5,8 +5,7 @@ const connectDatabase =()=>{
 
     mongoose.connect(process.env.MONGODB_URL,{useNewUrlParser:true,useUnifiedTopology:true}).then((data)=>{
         console.log(`Mongodb connected to database : ${data.connection.host}`);
-    }).catch((err)=>{
-    console.log(err)})
+    })
 
 }
 
--- backend/server.js
+++ backend/server.js
@@ -1,16 +1,33 @@
 const app = require("./app");
 
 const dotenv = require('dotenv')
-
 const connectDatabase = require('./config/database.js')
 
-// Config
+// Handling uncaught Exception ------------------------------------------------------
+process.on("uncaughtException",(err)=>{
+    console.log(`Error: ${err.message}`);
+    console.log('Shutting down the server due to uncaught Exception');
+    process.exit(1)
+})
+
+// Config -------------------------------------------------------
 dotenv.config({path:'backend/config/config.env'});
 
-// connecting to database
+// connecting to database -------------------------------------------------------
 
 connectDatabase()
 
-app.listen(process.env.PORT,()=>{
+const server =  app.listen(process.env.PORT,()=>{
     console.log(`Server running on http://localhost:${process.env.PORT}`);
+})
+
+// ---------------------------------------------------------------------------------------
+// Unhandled promise rejection ---> When mongoDb url error occured
+process.on("unhandledRejection",(err)=>{
+    console.log(`Error : ${err.message}`);
+    console.log('Shutting down the server due to unhandled Promise Rejection');
+
+    server.close(()=>{
+        process.exit(1)
+    })
 })
